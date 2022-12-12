3731d2182bfdd368309ad7d1206e2d651fdf5f7d
ryan <ryanww86@gmail.com>
2022-12-12 09:41:01 -0500

    add list_data in view and url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    
+    
+    # post/ means that the URL should begin with the word post followed by a /. So far so good.
+    # <int:pk> – this part is trickier. It means that Django expects an integer value and will transfer it to a view as a variable called pk.
+    # /  then we need a / again before finishing the URL.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
