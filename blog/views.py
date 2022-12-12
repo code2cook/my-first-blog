dcb8ad37ae2c5115f69a7fe5bc596f0bd7cf3ee5
ryan <ryanww86@gmail.com>
2022-12-11 20:03:42 -0500

    add posts filter to fidn post by the order of published_date

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 # Create your views here.
 # view is used to connect models and templates.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
